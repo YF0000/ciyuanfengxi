15d0553d1edcba105d49563ade84b3dd70a97f28
YF <yf2452568559@gmail.com>
2023-05-15 19:48:32 +0800

    更新了post页面

--- src/components/publicvue/post.vue
+++ src/components/publicvue/post.vue
@@ -1,40 +1,79 @@
 <template>
     <div class="post">
-        <div class="tiezi">
-            <div class="user">
-                <el-avatar :size="30" src="https://p.qqan.com/up/2021-1/16100008745562727.jpg" @error="errorHandler">
-                    <img src="https://p.qqan.com/up/2021-1/16100008745562727.jpg" />
-                </el-avatar>
-                <span>username</span>
-            </div>
-            <div class="title">
-                <p>标题💟💝💕💞💓</p>
-            </div>
-            <div class="T-img">
-                <div>
-                    <a href="/item/detail/7228945602476776485?_source_page=home">
-                    <img
-                src="https://p3-bcy-sign.bcyimg.com/banciyuan/c0290b689f004a77b3a6062241d072f7~tplv-banciyuan-sq520.image?x-expires=1699270723&amp;x-signature=x3VKkpZCEeEmD9bhTyvtJsATd68%3D"
-                ></a></div>
-            </div>
-            <i class="el-icon-star-off">收藏</i>
-            <i class="el-icon-chat-line-round">评论</i>
-            <i class="el-icon-share">转发</i>
-            <i>❤点赞</i>
-        </div>
+        <ul>
+            <li class="list letf_list">
+                <div class="tiezi" v-for="(item,index) in dataList" :key="item.id" v-if="index%2===0">
+                    <div class="user">
+                        <el-avatar :size="30" src="https://p.qqan.com/up/2021-1/16100008745562727.jpg"
+                            @error="errorHandler">
+                            <img src="https://p.qqan.com/up/2021-1/16100008745562727.jpg" />
+                        </el-avatar>
+                        <span>username</span>
+                    </div>
+                    <div class="title">
+                        <p>{{ item.postTitle }}</p>
+                    </div>
+                    <div class="T-img">
+                        <div>
+                            <a href="#">
+                                <img
+                                    :src=" item.postCoverUrl "></a>
+                        </div>
+                    </div>
+                    <i class="el-icon-star-off">收藏</i>
+                    <i class="el-icon-chat-line-round">评论</i>
+                    <i class="el-icon-share">转发</i>
+                    <i>❤点赞</i>
+                </div>
+            </li>
+            <li class="list right_list">
+                <div class="tiezi" v-for="(item,index) in dataList" :key="item.id" v-if="!(index%2===0)">
+                    <div class="user">
+                        <el-avatar :size="30" src="https://p.qqan.com/up/2021-1/16100008745562727.jpg"
+                            @error="errorHandler">
+                            <img src="https://p.qqan.com/up/2021-1/16100008745562727.jpg" />
+                        </el-avatar>
+                        <span>username</span>
+                    </div>
+                    <div class="title">
+                        <p>{{ item.postTitle }}</p>
+                    </div>
+                    <div class="T-img">
+                        <div>
+                            <a href="#">
+                                <img
+                                    :src=" item.postCoverUrl "></a>
+                        </div>
+                    </div>
+                    <i class="el-icon-star-off">收藏</i>
+                    <i class="el-icon-chat-line-round">评论</i>
+                    <i class="el-icon-share">转发</i>
+                    <i>❤点赞</i>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import service from '@/service';
 export default {
     name: 'post',
 
     data() {
         return {
             isTwo: 1,
+            dataList:[]
         };
     },
-
+    created(){
+        this.getAll();
+    },
+    computed:{
+        getEgg(){
+            console.log(this.dataList);
+        }
+    },
     mounted() {
 
     },
@@ -43,40 +82,74 @@ export default {
         errorHandler() {
             return true
         },
+        getAll(){
+            service.get("/posts").then(res=>{
+                console.log(res.data);
+                this.dataList=res.data
+            })
+        }
     },
 };
 </script>
 
 <style scoped>
-.user{
+.user {
+    margin: 10px 10px;
     text-align: left;
-    margin-left: 10px;
 }
+
 .user * {
     margin: 0 3px;
     vertical-align: middle;
 }
-.title{
+
+.title {
     font-family: "Lucida Console Emoji", Courier, monospace;
 }
+
 .tiezi {
     padding: 1% 1%;
-    width: 23%;
+    width: 98%;
     background: #ffffff;
-    margin: 20px auto;
     border-radius: 8px;
+    margin: 20px 0;
 }
+
 i {
     margin: 0 15px;
 }
-.T-img{
+
+.T-img {
     margin: 10px auto;
     width: 98%;
 }
-.T-img img{
+
+.T-img img {
     width: auto;
     height: auto;
     max-width: 100%;
     max-height: 100%;
+}
+ul{
+    width: 50%;
+    margin: 0 auto;
+}
+ul:after{
+    display: block;
+    content: "";
+    clear: both;
+}
+.list{
+    display: block;
+    float: left;
+    width: 47.9%;
+    margin: 20px 10px;
+}
+
+.list_left{
+    
+}
+.list_right{
+    
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    port: 8083,
+    open: true, //启动项目自动打开页面
+    proxy:{//配置跨域处理
+      '/api':{
+        target:'http://localhost:1212/',//前缀
+        changOrigin:true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    }
+  },
 })
